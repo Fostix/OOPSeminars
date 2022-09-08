11afc917d72c6373c22cfc851b2253d3c91baec2
albert shaymardanov <accretiondiskthat@gmail.com>
2022-09-08 09:40:04 +0500

    readme add first homework

--- src/main/java/MyExamples/Program002/Program.java
+++ src/main/java/MyExamples/Program002/Program.java
@@ -7,8 +7,9 @@ public class Program {
     public static void main(String[] args) {
         Animal c = new Cat((byte) 2, (byte) 2, "green", "gray", (byte) 4, (byte) 2, "Бармалей");
         System.out.println(c);
-        Shorts shorts = new Shorts();
-        Clothing jeans = new Shorts();
+        Clothing shorts = new Shorts();
         List<String> capboard = new ArrayList<>();
+        //System.out.println(shorts);
+        System.out.println(capboard);
     }
 }
